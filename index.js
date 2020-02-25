79790f80ce53614d698a54770a3f04d04bbc0ddf
Chad Simpson <mrsimpson3000@gmail.com>
2020-02-24 20:10:14 -0500

    Finished getModelYears

--- index.js
+++ index.js
@@ -295,8 +295,12 @@ console.log(sortCarInventory(inventory));
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
  */
-function getModelYears( /* code here */ ) {
-  /* code here */
+function getModelYears(inventory) {
+  let allYears = [];
+  for (let i = 0; i < inventory.length; i++) {
+    allYears.push(inventory[i].car_year);
+  }
+  return allYears;
 }
 
 /**
